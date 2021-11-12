c99a7910fb3021de5d8969b9d0cd1dae6aa19cdf
Laxman2916 <89543411+Laxman2916@users.noreply.github.com>
2021-11-11 19:45:00 -0600

    added requirements

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -32,3 +32,38 @@ exports.vehicle_delete = function(req, res) {
 exports.vehicle_update_put = function(req, res) { 
     res.send('NOT IMPLEMENTED: vehicle update PUT' + req.params.id); 
 }; 
+
+// VIEWS 
+// Handle a show all view 
+exports.vehicle_view_all_Page = async function(req, res) { 
+    try{ 
+        thevehicle = await vehicle.find(); 
+        res.render('vehicle', { title: 'vehicle Search Results', results: thevehicle }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+
+// Handle vehicle create on POST. 
+exports.vehicle_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new vehicle(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"vehicle_type":"goat", "cost":12, "size":"large"} 
+    document.Brand = req.body.Brand; 
+    document.price = req.body.price; 
+    document.size = req.body.size; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+ 
