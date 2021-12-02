1e6aab5d39f75fba291f00a8ee6f93c6082d0764
Laxman2916 <89543411+Laxman2916@users.noreply.github.com>
2021-12-01 22:14:05 -0600

    added requirements

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose")
-const vehicleSchema = mongoose.Schema({Brand: String,price: Number,size: String})
+const vehicleSchema = mongoose.Schema({Brand: String,price: {type:Number,min:15,max:100},size: String})
 module.exports = mongoose.model("vehicle",vehicleSchema)
